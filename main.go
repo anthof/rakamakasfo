397eacd34a56105a7c2b63d9463422dda7f4020a
anthof <anthofweb@gmail.com>
2018-01-22 21:27:30 +0200

    second no work

--- main.go
+++ main.go
@@ -9,11 +9,6 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-var (
-	db, _    = sql.Open("sqlite3", "cache/rakamakasfo.db")
-	createDB = "create table if not exist links (title text, description text, url text)"
-)
-
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 	t, err := template.ParseFiles("templates/index.html")
@@ -33,28 +28,35 @@ func writeLinkHandler(w http.ResponseWriter, r *http.Request) {
 	t.ExecuteTemplate(w, "writelink", nil)
 }
 
-func addLinkHandler(w http.ResponseWriter, r *http.Request) {
+func addLink(w http.ResponseWriter, r *http.Request) {
+	db, _ := sql.Open("sqlite3", "webdata.db")
+	if r.Method == "POST" {
+		r.ParseForm()
+		title := r.PostFormValue("title")
+		description := r.PostFormValue("description")
+		url := r.PostFormValue("url")
+		tx, _ := db.Begin()
+		stmt, _ := tx.Prepare("insert into links (title, description, url) values (?, ?, ?)")
+		_, err := stmt.Exec(title, description, url)
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+		}
+		tx.Commit()
+		db.Close()
+	} else {
+		t, err := template.ParseFiles("templates/writelink.html")
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+		}
 
-	title := r.FormValue("title")
-	description := r.FormValue("description")
-	url := r.FormValue("url")
-
-	db.Exec(createDB)
-	tx, _ := db.Begin()
-	stmt, _ := tx.Prepare("insert into links (title, description, url) values (?, ?, ?)")
-	_, err := stmt.Exec(title, description, url)
-	if err != nil {
-		fmt.Fprintf(w, err.Error())
+		t.ExecuteTemplate(w, "writelink", nil)
 	}
-	tx.Commit()
-	db.Close()
 }
 
 func main() {
 	fmt.Println("Listening on port :8000")
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/writelink", writeLinkHandler)
-	http.HandleFunc("/addlink", addLinkHandler)
+	http.HandleFunc("/addlink", addLink)
 	http.ListenAndServe(":8000", nil)
-
 }
